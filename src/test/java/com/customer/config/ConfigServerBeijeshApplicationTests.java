b78b2780e0942cbf45f9c440428909113259201a
brijesh9918 <brijesh9918@rediffmail.com>
2020-10-08 22:20:24 -0400

    first commit

---
+++ src/test/java/com/customer/config/ConfigServerBeijeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerBeijeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
